ccac3df4d333fc923a38f847d197b38ba6d600d3
ShaheerShahzadOfficial <mohammedshaheershahzadrawda5@gmail.com>
2023-07-02 20:35:33 +0500

    even odd added

--- app.js
+++ app.js
@@ -3,15 +3,26 @@ const numChecker = () => {
     let error = document.getElementById("error")
     const newNum = Number(number?.value)
     if (newNum !== NaN) {
-        if (newNum > 0) {
-            error.innerHTML = "Positive Number"
-        } else if (newNum < 0) {
-            error.innerHTML = "Negative Number"
+        if (newNum >= 0) {
+            if (newNum % 2 === 0) {
+                error.innerHTML = "Positive And Even Number"
 
-        } if (newNum === 0) {
-            error.innerHTML = "Number is zero"
+            } else {
+                error.innerHTML = "Positive And ODD Number"
+            }
+
+        } else if (newNum < 0) {
+            if (newNum % 2 === 0) {
+                error.innerHTML = "Negative And Even Number"
+
+            } else {
+                error.innerHTML = "Negative And ODD Number"
+            }
         }
     }
 
     number.value = ""
+    setTimeout(() => {
+        error.innerHTML = ""
+    }, 3000);
 }
